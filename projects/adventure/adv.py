f2ef578808ba7450af73c16305cbe89ff127e4ab
Remon Abdou <remoo1901@hotmail.com>
2020-10-25 20:15:04 -0400

    finished sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,14 +10,14 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,7 +29,43 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# to reverse directions
+inverse_directions = {"n": "s", "s": "n", "e": "w", "w": "e"}
 
+# PLAN:
+# set up direction for player
+# loop thru all the exits
+# set up if player has visited the current room to turn around
+# else if the room hasnt been visited to add the new path
+# and update the status to the new one using recursion
+# return the path
+# fill in the traversal path with the bfs func
+
+
+def bfs(starting_room, visited=set()):
+    # tracking the new path in a list
+    path = []
+    # looping thru the get_exits to find all exits
+    for direction in player.current_room.get_exits():  # using get exits from room py
+        player.travel(direction)
+        # if player is in a room thats visted
+        if player.current_room in visited:
+            # then turn to another direction by using inverse_direct
+            player.travel(inverse_directions[direction])
+        else:  # if the room has not been visited
+            # add the new path
+            visited.add(player.current_room)
+            path.append(direction)  # the new path has been added
+            # recurse to update current status
+            path = path + bfs(player.current_room, visited)
+            player.travel(inverse_directions[direction])
+            path.append(inverse_directions[direction])
+
+    return path  # return the path
+
+
+# fills in the list w the bfs func
+traversal_path = bfs(player.current_room)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,11 +77,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,13 +1,15 @@
+import random
+​
 class User:
     def __init__(self, name):
         self.name = name
-
+​
 class SocialGraph:
     def __init__(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-
+​
     def add_friendship(self, user_id, friend_id):
         """
         Creates a bi-directional friendship
@@ -19,7 +21,7 @@ class SocialGraph:
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
-
+​
     def add_user(self, name):
         """
         Create a new user with a sequential integer ID
@@ -27,15 +29,13 @@ class SocialGraph:
         self.last_id += 1  # automatically increment the ID to assign the new user
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
-
+​
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -43,28 +43,46 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
         # Add users
-
+        for i in range(0, num_users):
+            self.add_user(f"User {i}")
+​
         # Create friendships
-
+        # Generate all possible friendship combinations
+        possible_friendships =  []    #[ (Friend_id_1, friend_id_2 )  ]
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append( (user_id, friend_id) )
+        
+        # randomize the above array
+        random.shuffle(possible_friendships)
+​
+        # Pick out num_users * avg_friendships number of friend combos from possible_friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+            
+​
+​
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
         return visited
-
-
+​
+​
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+Collapse
+
+
+
